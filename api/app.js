04f7ff7c14c1b6aa2e4182e65b8eab5a164eab69
ktapia576 <ktapia576@hotmail.com>
2020-10-19 20:50:28 -0400

    Add logger for when someone requests data

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ const app = express();
 
 // Routes that should handle request
 app.get('/orders', (req, res, next) => {
+    console.log('Someone requested Data!');
     res.json(['Grape','Orange','Apple','Watermelon']);
 });
 
