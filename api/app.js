c7d9d19df2ac02cccbb2e254e39958c0eef2f79b
ktapia576 <ktapia576@hotmail.com>
2020-10-19 22:42:03 -0400

    Add mock api server so other server can request json data

--- api/app.js
+++ api/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 
-// Routes that should handle request
+const port = process.env.PORT || 3001;
+
+app.use(express.json()); // Allows to parse JSON since server will be sending and receiving JSON
+
+// Endpoints that should handle request
 app.get('/orders', (req, res, next) => {
     console.log('Someone requested Data!');
-    res.json(['Grape','Orange','Apple','Watermelon']);
+    // res.json(['Grape','Orange','Apple','Watermelon']);
+    res.status(200).send(['Grape','Orange','Apple','Watermelon']);
 });
 
-module.exports = app; // export app
+app.listen(port, () => {
+    console.log(`Server started successfully on PORT: ${port}`);
+});
